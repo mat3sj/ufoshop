67e04e361f8329fdb305c86d0862dd44458ffc9a
mat3sj <martin.jasan@monitora.cz>
2025-03-06 19:52:58 +0100

    some polishing

--- ufo_shop/models.py
+++ ufo_shop/models.py
@@ -1,11 +1,5 @@
-# from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
 from django.db import models
-
-#
-# class User(AbstractUser):
-#     phone = models.CharField(max_length=20, blank=True)
-#     is_merchandiser = models.BooleanField(default=False)
 
 
 class Location(models.Model):

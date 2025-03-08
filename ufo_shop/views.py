a6c7fda8bc3d62cde10624e683c1d408a1f9a58e
mat3sj <martin.jasan@monitora.cz>
2025-03-08 16:28:19 +0100

    some changes

--- ufo_shop/views.py
+++ ufo_shop/views.py
@@ -20,22 +20,23 @@ class ItemListView(ListView):
     def get_queryset(self):
         return Item.objects.filter(is_active=True)
 
-    # def get_queryset(self):
-    #     queryset = super().get_queryset().filter(is_active=True)  # Only active items
-    #     category = self.request.GET.get('category')
-    #     user = self.request.GET.get('user')
-    #
-    #     if category:
-    #         queryset = queryset.filter(category__id=category)
-    #     if user:
-    #         queryset = queryset.filter(merchandiser__id=user)
-    #
-    #     return queryset.distinct()
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(is_active=True)  # Only active items
+        category = self.request.GET.get('category')
+        # user = self.request.GET.get('user')
+
+        if category:
+            queryset = queryset.filter(category__id=category)
+        # Merchandiser filter
+        # if user:
+        #     queryset = queryset.filter(merchandiser__id=user)
+
+        return queryset.distinct()
     #
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()  # Pass categories to the template
-        context['users'] = User.objects.filter(item__isnull=False, is_staff=True).distinct()  # Merchandisers with items
+        # context['users'] = User.objects.filter(item__isnull=False, is_staff=True).distinct()  # Merchandisers with items
         return context
 
 

d4b6054a871cf5628da94828c0e1c418d4401888
mat3sj <martin.jasan@monitora.cz>
2025-08-19 10:10:14 +0200

    Migrate media file references to static files and configure `STATICFILES_DIRS` for consistent asset handling across templates and settings.

--- ufo_shop/settings/base.py
+++ ufo_shop/settings/base.py
@@ -135,7 +135,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# Include project-level static directory so collectstatic finds icons/fonts
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'

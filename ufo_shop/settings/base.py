0a39210ba092578e94e83f81ae4fe55294f18a5f
mat3sj <martin.jasan@monitora.cz>
2025-09-04 16:42:19 +0200

    Add localized invoice template, improve home page translations, and integrate i18n context processor
    Introduced a Czech-localized invoice PDF template with dynamic invoice and order details. Enhanced the home page with translatable text for improved localization support. Added `django.template.context_processors.i18n` to settings for enabling translation functionality across the project.

--- ufo_shop/settings/base.py
+++ ufo_shop/settings/base.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.template.context_processors.media',
+                'django.template.context_processors.i18n',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
